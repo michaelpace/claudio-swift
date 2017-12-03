7a51caf3c67867fed438ec6da6bd691ba733f625
Michael Pace <mpace1027@gmail.com>
2017-12-03 18:04:42 -0500

    first pass for the data store

--- Claudio/Platform/Data store/DataStore.swift
+++ Claudio/Platform/Data store/DataStore.swift
@@ -9,5 +9,54 @@
 import Foundation
 import RealmSwift
 
-/// TODO
-final class DataStore: DataStoring {}
+/// A data store for storing model objects.
+final class DataStore: DataStoring {
+
+    func write(_ block: () -> Void) {
+        do {
+            try realm.write(block)
+        } catch {
+            Logger.log(.error, "Error performing a write block: \(error.localizedDescription)")
+        }
+    }
+
+    func retrieveObjects<TypeToRetrieve: Object>(type: TypeToRetrieve.Type, filter: NSPredicate? = nil) -> [TypeToRetrieve] {
+        let results: Results<TypeToRetrieve> = {
+            if let filter = filter {
+                return realm.objects(type).filter(filter)
+            } else {
+                return realm.objects(type)
+            }
+        }()
+
+        return results.map { $0 }
+    }
+
+    func delete(_ model: Object) {
+        write {
+            realm.delete(model)
+        }
+    }
+
+    func delete(_ models: [Object]) {
+        write {
+            realm.delete(models)
+        }
+    }
+
+}
+
+// MARK: - Private
+
+private extension DataStore {
+
+    var realm: Realm! {
+        do {
+            return try Realm()
+        } catch {
+            Logger.log(.error, "Error creating a `Realm` instance: \(error.localizedDescription)")
+            return nil
+        }
+    }
+
+}
--- Claudio/Platform/Data store/DataStoring.swift
+++ Claudio/Platform/Data store/DataStoring.swift
@@ -7,12 +7,32 @@
 //
 
 import Foundation
+import RealmSwift
 
-/// TODO
+/// Describes a type which has methods to store, retrieve, and delete data models.
 protocol DataStoring {
 
-    // TODO: Start here. Define a protocol which will serve as a contract for any database solution.
-
+    /// Updates the contents of the data store per the changes in `block`.
+    ///
+    /// - Parameter block: A block that will be executed to update the data store.
     func write(_ block: () -> Void)
+
+    /// Retrieves objects of the specified type, optionally filtered with `filter`.
+    ///
+    /// - Parameters:
+    ///   - type: The type of the objects to retrieve.
+    ///   - filter: An optional filter to use when retrieving models.
+    /// - Returns: An array of objects of type `type` which match `filter` if it's not `nil`.
+    func retrieveObjects<TypeToRetrieve: Object>(type: TypeToRetrieve.Type, filter: NSPredicate?) -> [TypeToRetrieve]
+
+    /// Deletes the object from the data store.
+    ///
+    /// - Parameter model: The object to delete from the data store.
+    func delete(_ model: Object)
+
+    /// Deletes the objects from the data store.
+    ///
+    /// - Parameter models: The objects to delete from the data store.
+    func delete(_ models: [Object])
 
 }
