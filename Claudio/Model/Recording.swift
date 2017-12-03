f5d1a4e27c8c493b2fe1b541f5d456765b7a8df3
Michael Pace <mpace1027@gmail.com>
2017-12-03 18:17:23 -0500

    begin to fill out Recording model

--- Claudio/Model/Recording.swift
+++ Claudio/Model/Recording.swift
@@ -9,6 +9,38 @@
 import Foundation
 import RealmSwift
 
+/// Model object representing an audio recording.
 final class Recording: Object {
+
+    // MARK: - Properties
+
+    /// The recording's primary key.
+    @objc dynamic var identifier = 0
+
+    /// The URL to the recording's directory.
+    @objc dynamic var directory = ""
+
+    /// The recording's path on disk.
+    @objc dynamic var path = ""
+
+    override static func primaryKey() -> String? {
+        return "identifier"
+    }
+
+}
+
+// MARK: - Convenience properties
+
+extension Recording {
+
+    /// A `URL` representation of the recording's directory.
+    var directoryURL: URL {
+        if let url = URL(string: directory) {
+            return url
+        } else {
+            Logger.log(.error, "Error creating a URL from directory: \(directory)")
+            return URL(fileURLWithPath: "")
+        }
+    }
 
 }
